ff2101ea467e9df0fd3a33f0c21795fdc3fe3130
Mahidhar Venna <mahidhar.reddy@fxinnovation.com>
2021-02-17 17:40:04 +0530

    refact/grafana-module

--- README.md
+++ README.md
@@ -11,3 +11,18 @@ This repository follows [Semantic Versioning 2.0.0](https://semver.org/)
 
 ## Git Hooks
 This repository uses [pre-commit](https://pre-commit.com/) hooks.
+
+##Usage
+
+module "grafana" {
+  #https://github.com/grafana/grafana
+  source = "https://scm.dazzlingwrench.fxinnovation.com/fxinnovation-public/terraform-module-kubernetes-grafana"
+  # grafana_ingress_host = var.grafana_ingress_host
+  monitoring_name_space = var.monitoring_name_space
+  grafana_service_type = var.grafana_service_type
+  grafana_replica = var.grafana_replica
+  grafana_node_port = var.grafana_node_port
+  grafana_persistent_volume_claim_storage = var.grafana_persistent_volume_claim_storage
+  storage_class_name= var.storage_class_name
+
+}
--- main.tf
+++ main.tf
@@ -1,314 +1,121 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 1.13"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.0"
-    }
-  }
-}
+resource "kubernetes_deployment" "deployment" {
+  depends_on = [kubernetes_config_map.configmap, kubernetes_persistent_volume_claim.pvc]
 
-locals {
-  chart_name    = "grafana"
-  chart_version = var.chart_version
-  release_name  = "grafana"
-  namespace     = var.namespace
-  repository    = "https://grafana.github.io/helm-charts"
-  bucket_prefix = "grafana"
-  bucket_name   = module.s3_bucket.this_s3_bucket_id
-  role_name     = local.bucket_name
-  provider_url  = replace(var.oidc_provider_issuer_url, "https://", "")
-
-  grafana_ini = {
-    server = {
-      domain         = var.root_domain
-      root_url       = "https://${var.root_domain}"
-      enforce_domain = true
-    }
-
-    database = {
-      type     = "postgres"
-      host     = "${module.db.this_db_instance_address}:${module.db.this_db_instance_port}"
-      name     = "grafana"
-      user     = module.db.this_db_instance_username
-      ssl_mode = "require"
-    }
-
-    auth = {
-      disable_login_form = var.auth_disable_login_form
-      oauth_auto_login   = var.oauth_auto_login
-    }
-
-    "auth.basic" = {
-      enabled = var.auth_enable_basic
-    }
-
-    "external_image_storage.s3" = {
-      bucket = local.bucket_name
-      region = module.s3_bucket.this_s3_bucket_region
-    }
-  }
-
-
-  values = {
-    ingress = {
-      enabled = var.ingress_enabled
-      hosts   = var.ingress_hostnames
-
-      annotations = {
-        "kubernetes.io/ingress.class" : var.ingress_class
-        "cert-manager.io/cluster-issuer" : var.ingress_cluster_issuer
-      }
-
-      tls = [
-        {
-          hosts      = var.ingress_hostnames
-          secretName = "grafana-ingress-cert"
-        }
-      ]
-    }
-
-    envFromSecret = kubernetes_secret.grafana.metadata[0].name
-
-    serviceAccount = {
-      annotations = {
-        "eks.amazonaws.com/role-arn" = module.iam.this_iam_role_arn
-      }
-    }
-
-    plugins = []
-
-    datasources = {
-      "datasources.yaml" = {
-        apiVersion = 1
-        datasources : var.datasources
-      }
-    }
-
-    "grafana.ini" = merge(local.grafana_ini, var.oauth_config)
-  }
-}
-
-data aws_region "grafana" {}
-data aws_caller_identity "grafana" {}
-
-module "iam" {
-  source = "github.com/terraform-aws-modules/terraform-aws-iam//modules/iam-assumable-role-with-oidc?ref=v3.6.0"
-
-  create_role                   = true
-  role_name                     = "${local.release_name}-irsa-${random_id.grafana_rds.dec}"
-  provider_url                  = local.provider_url
-  oidc_fully_qualified_subjects = ["system:serviceaccount:${local.namespace}:${local.release_name}"]
-  tags                          = var.tags
-}
-
-data "aws_iam_policy_document" "grafana" {
-  statement {
-    actions = [
-      "s3:ListBucket",
-      "s3:PutObject",
-      "s3:GetObject"
-    ]
-
-    resources = [module.s3_bucket.this_s3_bucket_arn, "${module.s3_bucket.this_s3_bucket_arn}/*"]
-  }
-}
-
-resource "aws_iam_role_policy" "grafana" {
-  name = local.bucket_name
-  role = module.iam.this_iam_role_name
-
-  policy = data.aws_iam_policy_document.grafana.json
-}
-
-resource "aws_iam_role_policy_attachment" "additional" {
-  count = length(var.additional_irsa_role_policy_arns)
-
-  role       = module.iam.this_iam_role_name
-  policy_arn = var.additional_irsa_role_policy_arns[count.index]
-}
-
-module "s3_bucket" {
-  source = "github.com/terraform-aws-modules/terraform-aws-s3-bucket?ref=v1.17.0"
-
-  bucket_prefix = local.bucket_prefix
-  acl           = "private"
-  force_destroy = true
-
-  versioning = {
-    enabled = false
-  }
-
-  tags = var.tags
-}
-
-resource "random_id" "grafana_rds" {
-  keepers = {
-    release_name = local.release_name
-  }
-
-  byte_length = 10
-}
-
-resource "aws_security_group" "grafana_rds" {
-  name_prefix = "grafana-rds"
-  vpc_id      = var.vpc_id
-}
-
-resource "random_password" "grafana_db_password" {
-  length  = 16
-  special = false
-}
-
-module "db" {
-  source = "github.com/terraform-aws-modules/terraform-aws-rds?ref=v2.20.0"
-
-  identifier                      = "grafana${random_id.grafana_rds.dec}"
-  engine                          = "postgres"
-  engine_version                  = "12.3"
-  instance_class                  = var.database_instance_type
-  allocated_storage               = var.database_storage_size
-  storage_encrypted               = false
-  name                            = "grafana${random_id.grafana_rds.dec}"
-  username                        = "grafana"
-  password                        = random_password.grafana_db_password.result
-  port                            = "5432"
-  vpc_security_group_ids          = [aws_security_group.grafana_rds.id]
-  maintenance_window              = "Mon:00:00-Mon:03:00"
-  backup_window                   = "03:00-06:00"
-  backup_retention_period         = 0
-  tags                            = var.tags
-  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
-  subnet_ids                      = var.database_subnets
-  family                          = "postgres12"
-  major_engine_version            = "12"
-  skip_final_snapshot             = var.database_skip_final_snapshot
-  final_snapshot_identifier       = var.database_final_snapshot_identifier
-  snapshot_identifier             = var.database_snapshot_identifier
-  deletion_protection             = false
-  auto_minor_version_upgrade      = var.database_auto_minor_version_upgrade
-}
-
-resource "aws_security_group_rule" "grafana-cluster-rules" {
-  from_port                = 0
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.grafana_rds.id
-  to_port                  = module.db.this_db_instance_port
-  type                     = "ingress"
-  source_security_group_id = var.source_security_group
-}
-
-resource "kubernetes_namespace" "grafana" {
-  count = local.namespace == "kube-system" ? 0 : 1
 
   metadata {
-    name = local.namespace
-    annotations = {
-      "ingress-whitelist" = "${var.root_domain}"
-    }
-    labels = {
-      "role/grafana" : "true"
-      "role/system" : "true"
-    }
-  }
-}
-
-resource "kubernetes_secret" "grafana" {
-  metadata {
-    name      = "${local.release_name}-credentials"
-    namespace = local.namespace
-  }
-
-  data = merge({
-    GF_DATABASE_PASSWORD = module.db.this_db_instance_password
-  }, var.config_secrets)
-
-  depends_on = [kubernetes_namespace.grafana]
-}
-
-data "kubernetes_secret" "grafana_secret" {
-  depends_on = [helm_release.grafana-deploy]
-  metadata {
-    namespace = kubernetes_namespace.grafana[0].metadata[0].name
     name      = "grafana"
-  }
-}
+    namespace = var.monitoring_name_space
+    labels = {
+      app = "grafana"
+    }
+    annotations = {
+      "configmap.reloader.stakater.com/reload" = "grafana-datasources"
+    }
 
-resource "kubernetes_job" "grafana_createdb" {
-  metadata {
-    name      = "grafana-createdb"
-    namespace = local.namespace
   }
 
   spec {
+    replicas = var.grafana_replica
+    selector {
+      match_labels = {
+        app = "grafana"
+      }
+    }
+    strategy {
+      type = "RollingUpdate"
+      rolling_update {
+        max_surge       = 1
+        max_unavailable = "33%"
+      }
+    }
+
     template {
       metadata {
-        annotations = {
-          "sidecar.istio.io/inject" = "false"
+        labels = {
+          app = "grafana"
         }
       }
 
       spec {
         container {
-          name  = "grafana-createdb"
-          image = "postgres:alpine"
-
-          env {
-            name  = "PGHOST"
-            value = module.db.this_db_instance_address
+          name  = "grafana"
+          image = "grafana/grafana:latest"
+          port {
+            name           = "grafana"
+            container_port = 3000
           }
 
-          env {
-            name  = "PGPORT"
-            value = module.db.this_db_instance_port
-          }
-
-          env {
-            name  = "PGDATABASE"
-            value = "postgres"
-          }
-
-          env {
-            name  = "PGUSER"
-            value = module.db.this_db_instance_username
-          }
-
-          env {
-            name = "PGPASSWORD"
-            value_from {
-              secret_key_ref {
-                name = kubernetes_secret.grafana.metadata[0].name
-                key  = "GF_DATABASE_PASSWORD"
-              }
+          resources {
+            limits {
+              cpu    = "1000m"
+              memory = "2Gi"
+            }
+            requests {
+              cpu    = "500m"
+              memory = "1Gi"
             }
           }
 
-          command = ["/bin/sh", "-c", "psql -tc \"SELECT 1 FROM pg_database WHERE datname = 'grafana'\" | grep -q 1 || psql -c 'CREATE DATABASE grafana'"]
+
+          volume_mount {
+            name       = "grafana-storage"
+            mount_path = "/var/lib/grafana"
+          }
+
+          volume_mount {
+            name       = "grafana-datasources"
+            mount_path = "/etc/grafana/provisioning/datasources"
+            read_only  = false
+
+          }
         }
+        volume {
+          name = "grafana-datasources"
+          config_map {
+            default_mode = "0777"
+            name         = "grafana-datasources"
+          }
+        }
+        volume {
+          name = "grafana-storage"
+          persistent_volume_claim {
+            claim_name = "grafana-pvc"
+          }
+        }
+        automount_service_account_token = true
+        # node_selector = {
+        #   type = "master"
+        # }
+        security_context {
+          fs_group = "472"
+        }
+
       }
     }
   }
-
-  depends_on = [kubernetes_namespace.grafana]
 }
 
-resource "helm_release" "grafana-deploy" {
-  name             = local.release_name
-  chart            = local.chart_name
-  version          = local.chart_version
-  repository       = local.repository
-  namespace        = local.namespace
-  create_namespace = true
 
-  wait   = var.wait
-  values = [yamlencode(local.values), yamlencode(var.chart_values)]
+resource "kubernetes_service" "service" {
+  metadata {
+    name      = "grafana"
+    namespace = var.monitoring_name_space
+    annotations = {
+      "prometheus.io/scrape" = "true"
+      "prometheus.io/port"   = "3000"
+    }
 
-  depends_on = [kubernetes_namespace.grafana]
+  }
+  spec {
+    selector = {
+      app = "grafana"
+    }
+    port {
+      port        = 3000
+      target_port = 3000
+      node_port   = var.grafana_node_port
+    }
+
+    type = var.grafana_service_type
+  }
 }
